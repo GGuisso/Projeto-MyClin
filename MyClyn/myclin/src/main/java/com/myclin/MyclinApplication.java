f5ab7fa6d752d212b07cdc35a4563f46fe01eafc
Guilherme Guisso <guisso.dev@gmail.com>
2022-04-14 17:26:38 -0300

    Iniciando projeto myclin

---
+++ MyClyn/myclin/src/main/java/com/myclin/MyclinApplication.java
@@ -0,0 +1,13 @@
+package com.myclin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyclinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyclinApplication.class, args);
+	}
+
+}
---
+++ MyClyn/myclin/src/test/java/com/myclin/MyclinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myclin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyclinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
